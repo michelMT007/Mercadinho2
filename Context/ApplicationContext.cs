553552f36693e611b15f35b3f6e88a82777ca796
Michel Costa <michel.costa@amaggi.com.br>
2021-10-13 16:58:45 -0400

    Primeiro commit para envio ao Github

---
+++ Context/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categoria { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MACDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Produto
+    {
+        public int  Id{ get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string BarCode { get; set; }
+        public decimal Preco { get; set; }
+        public Categoria FK { get; set; } 
+    }
+}
